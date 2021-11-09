25feadec2ac9cae1274585adf2eb6a38a89c8137
Alex Tinarsky <alex.tinarsky@mail.ru>
2021-11-09 11:32:02 +0300

    Removed commented code

--- SAT-problem_recursion/Program.cs
+++ SAT-problem_recursion/Program.cs
@@ -19,17 +19,11 @@ namespace SAT_problem_recursion
     }
     class BoolVariable : ExprBoolean
     {
-        /*public bool Value { get; set; }*/
         public HashSet<bool> PossibleValues { get; set; }
         public BoolVariable()
         {
             this.PossibleValues = new HashSet<bool>(new bool[] { true, false });
         }
-        /*public BoolVariable(bool val)
-        {
-            this.Value = val;
-            this.PossibleValues = new HashSet<bool>(new bool[] { true, false });
-        }*/
     }
 
     class BoolOp : ExprBoolean
@@ -47,53 +41,6 @@ namespace SAT_problem_recursion
 
     public class Program
     {
-        /*static void ExpressionToString(ExprBoolean exprTree, StringBuilder ExpressionString)
-        {
-            if (exprTree is BoolVariable)
-            {
-                ExpressionString.Append((exprTree as BoolVariable).Value);
-                return;
-            }
-            var opTree = exprTree as BoolOp;
-            switch (opTree.Op)
-            {
-                case BoolOperations.Not:
-                    ExpressionString.Append("!");
-                    ExpressionString.Append('(');
-                    ExpressionToString(opTree.Left, ExpressionString);
-                    ExpressionString.Append(')');
-                    break;
-                case BoolOperations.Or:
-                    ExpressionToString(opTree.Left, ExpressionString);
-                    ExpressionString.Append(" | ");
-                    ExpressionToString(opTree.Right, ExpressionString);
-                    break;
-                case BoolOperations.And:
-                    ParenthesesForOr(opTree.Left, ExpressionString);
-                    ExpressionString.Append(" & ");
-                    ParenthesesForOr(opTree.Right, ExpressionString);
-                    break;
-            }
-        }
-
-        static bool Evaluate(ExprBoolean exprTree)
-        {
-            if (exprTree is BoolOp)
-            {
-                var opTree = exprTree as BoolOp;
-                switch (opTree.Op)
-                {
-                    case BoolOperations.Not:
-                        return !(Evaluate(opTree.Left));
-                    case BoolOperations.Or:
-                        return (Evaluate(opTree.Left) | Evaluate(opTree.Right));
-                    case BoolOperations.And:
-                        return (Evaluate(opTree.Left) | Evaluate(opTree.Right));
-                }
-            }
-            return (exprTree as BoolVariable).Value;
-        }*/
-
         public static bool IsThereSolution(ExprBoolean exprTree, bool expectedValue)
         {
             if (exprTree is BoolOp)
@@ -122,20 +69,6 @@ namespace SAT_problem_recursion
             return boolVar.PossibleValues.Count > 0;
         }
 
-        /*static void ParenthesesForOr(ExprBoolean expr, StringBuilder ExpressionString)
-        {
-            if ((expr is BoolOp) && ((expr as BoolOp).Op == BoolOperations.Or))
-            {
-                ExpressionString.Append('(');
-                ExpressionToString(expr, ExpressionString);
-                ExpressionString.Append(')');
-            }
-            else
-            {
-                ExpressionToString(expr, ExpressionString);
-            }
-        }*/
-
         static void Main(string[] args)
         {
 
@@ -159,11 +92,6 @@ namespace SAT_problem_recursion
             {
                 Console.WriteLine(elem);
             }
-            /*StringBuilder ExpressionString = new StringBuilder();
-            ExpressionToString(Exp, ExpressionString);
-            Console.WriteLine(ExpressionString.ToString());
-
-            Console.WriteLine(Evaluate(Exp));*/
         }
     }
 }
